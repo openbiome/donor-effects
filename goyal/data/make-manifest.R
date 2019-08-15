5d20bf538cf562135e9d23b1722fff8095e8b754
Scott Olesen <swo@alum.mit.edu>
2019-08-15 16:22:34 -0400

    goyal

---
+++ goyal/data/make-manifest.R
@@ -0,0 +1,26 @@
+#!/usr/bin/env Rscript
+
+study_accession <- "PRJNA380944"
+
+filereport <- read_tsv(str_glue("https://www.ebi.ac.uk/ena/data/warehouse/filereport?accession={study_accession}&result=read_run")) %>%
+  # keep only donor samples
+  filter(str_detect(library_name, '\\.D\\d?$')) %>%
+  mutate(
+    url = str_c('ftp://', fastq_ftp),
+    filepath = str_c('fastq/', run_accession, '.fastq.gz'),
+    absolute_filepath = str_c('$PWD/', filepath)
+  )
+
+manifest <- filereport %>%
+  mutate(direction = 'forward') %>%
+  select(
+    `sample-id` = library_name,
+    `absolute-filepath` = absolute_filepath,
+    direction
+  )
+
+samples <- filereport %>%
+  select(filepath, url, fastq_md5)
+
+write_csv(manifest, 'manifest.csv')
+write_csv(samples, 'samples.csv')
