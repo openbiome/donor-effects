2246e2201b97e946a154cb4f0080dde6a184e8b1
Scott Olesen <swo@alum.mit.edu>
2019-09-05 12:51:04 -0400

    improved labels

--- power-calculation/make-plot.R
+++ power-calculation/make-plot.R
@@ -14,15 +14,15 @@ plot <- powers %>%
   mutate(label = if_else(
     is.na(effect_size),
     '',
-    percent(effect_size, accuracy = 1)
+    as.character(round(effect_size * 100))
   )) %>%
   ggplot(aes(factor(n_donors), factor(n_patients))) +
   geom_tile(aes(fill = effect_size)) +
   geom_text(aes(label = label), size = 8 * 25.4 / 72) +
   scale_fill_gradient(
-    name = '',
+    name = 'Effect size (p.p.)',
     limits = c(lower_bound, upper_bound + 1e-6),
-    labels = partial(percent, accuracy = 1),
+    labels = function(x) round(x * 100),
     low = 'white', high = 'red',
     na.value = 'gray75'
   ) +
@@ -33,7 +33,8 @@ plot <- powers %>%
   theme(
     panel.grid.major = element_blank(),
     axis.text = element_text(size = 8, color = 'black'),
-    legend.text = element_text(size = 8, color = 'black')
+    legend.text = element_text(size = 8, color = 'black'),
+    legend.title = element_text(size = 8, color = 'black')
   )
 
 ggsave('power-landscape.pdf', plot = plot, width = 4, height = 2.8, units = 'in')
