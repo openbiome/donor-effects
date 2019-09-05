4c1cf7347410674abff15d9e3ae63c852347e345
Scott Olesen <swo@alum.mit.edu>
2019-09-05 09:39:55 -0400

    nishida

---
+++ nishida/mhc.R
@@ -0,0 +1,9 @@
+#!/usr/bin/env Rscript --vanilla
+
+library(tidyverse)
+
+# Read in the data from Nishida et al. Table 4
+table4 <- read_tsv('table4.tsv')
+
+table4 %>%
+  mutate(MHC_significant = p.adjust(table4$p_value, 'BH') < 0.05)
