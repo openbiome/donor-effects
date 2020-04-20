870739978a4dfbbbd00816e5c75b05e83cb08ace
swo <swo@alum.mit.edu>
2020-04-20 12:04:35 -0500

    moved around

--- goyal/analyze.R
+++ goyal/analyze.R
@@ -2,7 +2,21 @@
 
 library(tidyverse)
 
-data <- read_tsv('data.tsv')
+# Read the diversity file produced by Qiime
+raw <- read_tsv('diversity-data/alpha-diversity.tsv', skip = 1, col_names = c('sample_id', 'diversity'))
+
+# Clean up the diversity table
+diversity <- raw %>%
+  mutate(patient_id = as.numeric(str_match(sample_id, 'FMT\\.0\\d\\.0*(\\d+)\\.D\\d*')[, 2])) %>%
+  group_by(patient_id) %>%
+  summarize_at('diversity', mean)
+
+# Load patient data
+patients <- read_tsv('patient-data.tsv')
+
+# Join and export data
+data <- patients %>%
+  left_join(diversity, by = 'patient_id')
 
 cat("\n\nSummary of diversity -----------------------------------------------\n")
 data %>%
